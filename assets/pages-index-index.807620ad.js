2e01c5492714061e665aad39ebd845c952dcf0b9
yangyangzaizai <87468293+yangyangzaizai@users.noreply.github.com>
2023-11-02 17:23:36 +0800

    Add files via upload

---
+++ assets/pages-index-index.807620ad.js
@@ -0,0 +1,1 @@
+import{o,c as e,i as t}from"./index-dd1a9fff.js";import{_ as n}from"./_plugin-vue_export-helper.1b428a4d.js";location.href="https://weixin.qq.com";const r=n({data:()=>({}),onLoad(){},onReady(){},methods:{}},[["render",function(n,r,a,s,i,d){const p=t;return o(),e(p)}]]);export{r as default};
