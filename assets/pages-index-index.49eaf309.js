4304ee123f82839f1893d3a4853dcb3dd90c829a
yangyangzaizai <87468293+yangyangzaizai@users.noreply.github.com>
2023-11-02 17:20:07 +0800

    Delete assets directory

--- assets/pages-index-index.49eaf309.js
+++
@@ -1,1 +0,0 @@
-import{o,c as e,i as t}from"./index-c395dde4.js";import{_ as n}from"./_plugin-vue_export-helper.1b428a4d.js";location.href="https://weixin.qq.com";const r=n({data:()=>({}),onLoad(){},onReady(){},methods:{}},[["render",function(n,r,a,s,i,d){const p=t;return o(),e(p)}]]);export{r as default};
